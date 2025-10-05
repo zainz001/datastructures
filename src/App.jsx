2c31c2bb29dde743da31fdfa0dae24e3b6457571
usama-xphyre <usama.hanif@xphyre,com>
2025-10-05 04:10:16 -0700

    Main frictional

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,34 @@ function App() {
 
 
 
-  function plaindrope(num) {
-    for (let i = 0; i = num.length / 2; i++) {
-      if (num[i] !== num[num.length - 1 - i]) {
-        return false, { num }
+   function plaindrope(str) {
+    // Loop only halfway through the string
+    for (let i = 0; i < str.length / 2; i++) {
+      // Compare characters from both ends
+      if (str[i] !== str[str.length - 1 - i]) {
+        return false;
       }
-      return true
-
     }
-
-
+    return true;
   }
-  console.log(plaindrope("racecar"));
+
+  // console.log(plaindrope("racecar")); // true
+  console.log(plaindrope("hello"));   // false
+
+  // console.log(plaindrope("racecar"));
+
+  // console.log(plaindrope("hello"));
+
+
+  function frictional(num) {
+    let result = 1
+    for (let i = 1; i < num; i++) {
+      result = result * i
+    }
+    return result
+  }
+  console.log(frictional(5));
   
-  console.log(plaindrope("hello"));
 
   return (
     <>
