761fc989f5c278032033ad6bd5085f962a56f003
usamasparkingasia <133225614+zainz001@users.noreply.github.com>
2025-10-30 01:03:52 -0700

    Main maxarray

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,19 @@ function App() {
 
 
 
-  function plaindrope(str) {
-    // Loop only halfway through the string
-    for (let i = 0; i < str.length / 2; i++) {
-      // Compare characters from both ends
-      if (str[i] !== str[str.length - 1 - i]) {
-        return false;
-      }
-    }
-    return true;
-  }
+  // function plaindrope(str) {
+  //   // Loop only halfway through the string
+  //   for (let i = 0; i < str.length / 2; i++) {
+  //     // Compare characters from both ends
+  //     if (str[i] !== str[str.length - 1 - i]) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  // }
 
   // console.log(plaindrope("racecar")); // true
-  console.log(plaindrope("hello"));   // false
+  // console.log(plaindrope("hello"));   // false
 
   // console.log(plaindrope("racecar"));
 
@@ -45,29 +45,42 @@ function App() {
   //   return b
 
   // }
-  // console.log(fibonacci(6));
-  function sumDigits(num) {
-    let sum = 0;
-    while (num > 0) {
-      sum = sum + num % 10;
-      num = Math.floor(num / 10);
-    }
-    return sum;
-  }
+  // console.log(fibonacci(7));
+  // function sumDigits(num) {
+  //   let sum = 0;
+  //   while (num > 0) {
+  //     sum = sum + num % 10;
+  //     num = Math.floor(num / 10);
+  //   }
+  //   return sum;
+  // }
   // console.log(sumDigits(1234)); // 10
 
 
-  function maxarray(arr) {
-    let max = arr[0]
-    for (let i = 1; i <= arr.length; i++) {
-      if (arr[i] > max) {
-        max=arr[i]
-      }
-    }
-    return max
-  }
-  console.log(maxarray([1,3,5,2,6,2]));
-  
+  // function maxarray(arr) {
+  //   let max = arr[0]
+  //   for (let i = 1; i <= arr.length; i++) {
+  //     if (arr[i] > max) {
+  //       max=arr[i]
+  //     }
+  //   }
+  //   return max
+  // }
+  // console.log(maxarray([1,3,5,2,6,2]));
+
+//   function largestarr(arr) {
+//     let result = []
+//     for (let i = 1; i <= arr.length; i++) {
+//       if (arr[i] > result) {
+//       result = arr[i]
+//       }
+//     }
+//     return result
+//   }
+// console.log(largestarr([1,5,3,4,2]));
+
+
+
   return (
     <>
       <h2>Check the console for output</h2>
